bca860a3910b030431ba0d35262af7b67102ac11
brother-samuel <samuel@hesperion.cz>
2025-06-09 22:58:27 +0200

    blackjack prelim

--- calc.py
+++ calc.py
@@ -21,17 +21,17 @@ result = None
 
 while calculating:
     if result == None:
-        n1 = int(input("What\'s the first number?: "))
+        n1 = float(input("What\'s the first number?: "))
     else:
         use_result = input(f"Type 'y' to continue calculating with {result} or type 'n' to start anew or 'q' to quit: ").lower()
         if use_result == "y":
             n1 = result
         elif use_result == "n":
-            n1 = int(input("What\'s the first number?: "))
+            n1 = float(input("What\'s the first number?: "))
         else:
             calculating = False
 
     operator = input("+\n -\n *\n /\n Pick an operation: ")
-    n2 = int(input("What\'s the next number?: "))
+    n2 = float(input("What\'s the next number?: "))
     result = operations[operator](n1, n2)
-    print(result)
+    print(f"{n1} {operator} {n2} = {result}")
