fe2a7938a5eed58d2f993a5bef262d730aca0141
brother-samuel <samuel@hesperion.cz>
2025-06-10 09:31:58 +0200

    v2
    cleaned up the code with defining functions.

--- blackjack.py
+++ blackjack.py
@@ -1,29 +1,36 @@
 import random
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-my_cards = []
-computer_cards = []
 
-for card in range(2):
-    my_cards.append(random.choice(cards))
-for card in range(2):
-    computer_cards.append(random.choice(cards))
-if sum(computer_cards) < 17:
-    computer_cards.append(random.choice(cards))
 
-#print(my_cards)
-#print(computer_cards)
+def show_drawn_cards():
+    print(f"Your cards: {my_cards}, current score: {sum(my_cards)}")
+    print(f"Computer's first card: {computer_cards[0]}")
+
+def draw_card(card_list, num_cards=1):
+    for card in range(num_cards):
+        card_list.append(random.choice(cards))
+
+def show_hands():
+    print(f"Your final hand: {my_cards}, final score: {sum(my_cards)}")
+    if sum(computer_cards) < 17:
+        computer_cards.append(random.choice(cards))
+    print(f"Computer's final hand: {computer_cards}, final score: {sum(computer_cards)}")
+
+def calculate_result():
+    ...
+
 
 play_blackjack = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
 while play_blackjack == "y":
-    print(f"Your cards: {my_cards}, current score: {sum(my_cards)}")
-    print(f"Computer's first card: {computer_cards[0]}")
-    another_my_card = input("Type 'y' to get another card, type 'n' to pass")
-    if another_my_card == "y":
-        my_cards.append(random.choice(cards))
-        print(f"Your cards: {my_cards}, current score: {sum(my_cards)}")
-        print(f"Computer's first card: {computer_cards[0]}")
-        another_my_card = input("Type 'y' to get another card, type 'n' to pass")
-    elif another_my_card == "n":
-        print(f"Your final hand: {my_cards}, final score: {sum(my_cards)}")
-        print(f"Computer's final hand: {computer_cards}, final score: {sum(computer_cards)}")
+    my_cards = []
+    computer_cards = []
+    draw_card(computer_cards, 2)
+    draw_card(my_cards,2)
+    show_drawn_cards()
+    while input("Type 'y' to get another card, type 'n' to pass").lower() == "y":
+        draw_card(my_cards, 1)
+        show_drawn_cards()
+    show_hands()
+    
+#        play_blackjack = "n"
         
