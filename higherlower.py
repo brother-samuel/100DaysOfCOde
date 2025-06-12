ab16d409ffd336c83b095dbe6cd9e78e0609aea4
brother-samuel <samuel@hesperion.cz>
2025-06-12 18:49:27 +0200

    Higher/lower game

---
+++ higherlower.py
@@ -0,0 +1,38 @@
+import random
+from game_data import data
+
+def pick_next_account(account_a):
+    account_B = random.randint(0, len(data) - 1)
+    while account_B == account_a:
+        account_B = random.randint(0, len(data) - 1)
+    return account_B
+
+def make_choice(account_a, account_b):
+    choice = input("Who has more followers? Type 'A' or 'B':  ").upper()
+    a_followers = int(data[account_a]['follower_count'])
+    b_followers = int(data[account_b]['follower_count'])
+    return ((choice == "A" and a_followers >= b_followers) or
+            (choice == "B" and b_followers >= a_followers))
+
+
+def main():
+    playing = True
+    account_a = random.randint(0, len(data) - 1)
+    score = 0
+    #current choice (A)
+    while playing:
+        print(f"Compare A: {data[account_a]['name']}, {data[account_a]['description']} from {data[account_a]['country']}")
+        print("vs.")
+        #pick next choice (B)
+        account_b = pick_next_account(account_a)
+        print(f"Against B: {data[account_b]['name']}, {data[account_b]['description']} from {data[account_b]['country']}")
+        if make_choice(account_a,account_b):
+            score += 1
+            account_a = account_b
+            print(f"You're right! Current score: {score}.")
+        else:
+            playing = False
+            print(f"Sorry, that's wrong. Final score: {score}.")
+
+if __name__ == "__main__":
+    main()
