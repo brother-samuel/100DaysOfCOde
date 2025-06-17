8bf431b89f11c224513d3f72c66a330614a44ac0
brother-samuel <samuel@hesperion.cz>
2025-06-17 12:28:23 +0200

    timmy doing square

---
+++ Day18/main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+
+
+screen = Screen()
+screen.exitonclick()
