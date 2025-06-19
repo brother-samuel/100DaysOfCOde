1620822bd9c5fb225c158887146c243d549a486d
brother-samuel <samuel@hesperion.cz>
2025-06-19 22:25:35 +0200

    Create spirgoraph_v2.py

---
+++ Day18/spirgoraph_v2.py
@@ -0,0 +1,31 @@
+from re import S
+import turtle as t
+import random
+
+tim = t.Turtle()
+
+t.colormode(255)
+tim.speed(0)
+tim.pensize(1)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+# degrees = 5 
+# for _ in range(72):
+#     tim.seth(degrees)
+#     tim.pencolor(random_color())
+#     tim.circle(100)
+#     degrees += 5
+    
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100) 
+        tim.setheading(tim.heading() + size_of_gap)
+
+
+draw_spirograph(3)
