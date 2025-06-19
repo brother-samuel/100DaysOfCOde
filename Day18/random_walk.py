0b0406771ce5e80aa989a8d51e5c44a39c8a22aa
brother-samuel <samuel@hesperion.cz>
2025-06-19 21:59:15 +0200

    Update random_walk.py

--- Day18/random_walk.py
+++ Day18/random_walk.py
@@ -1,1 +1,15 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen", "MediumSlateBlue", "DarkSeaGreen", "LightSteelBlue", "PaleVioletRed", "CadetBlue", "DarkKhaki"]
+directions = [0, 90, 180, 270]
+tim.speed(0)
+tim.pensize(10)
+
+for _ in range(300):
+    tim.seth(random.choice(directions))
+    tim.pencolor(random.choice(colours))
+    tim.forward(30)
 
