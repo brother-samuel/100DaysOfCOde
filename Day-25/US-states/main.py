6c91facdb0eff3fde6b871d04c0add9e224ae8b5
brother-samuel <samuel@hesperion.cz>
2025-06-26 20:30:21 +0200

    US states game small tweaks

--- Day-25/US-states/main.py
+++ Day-25/US-states/main.py
@@ -1,17 +1,17 @@
 import turtle
-import random
 import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
-screen.screensize(800, 600)
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
-score = 0
+
 states = pandas.read_csv("50_states.csv")
 states_list = states['state'].tolist()
 # print(states_list)
+
+score = 0
 quiz_on = True
 guessed_states = []
 
@@ -23,10 +23,15 @@ writer.speed("fastest")
 while quiz_on:
     answer_state = screen.textinput(title=f"{score}/50 States Correct", prompt="Type in the name of a state:")
     # print(answer_state)
-    answer_state = answer_state.title()
+    if answer_state is None:
+        missed_states = list(set(states_list) - set(guessed_states))
+        pandas.Series(missed_states).to_csv("missed_states.csv")
+        quiz_on = False
+        break
     if score == 50:
         print("You have guessed all 50 states!")
         quiz_on = False
+    answer_state = answer_state.title()
     if answer_state in states_list:
         guessed_states.append(answer_state)
         score += 1
@@ -36,9 +41,7 @@ while quiz_on:
         write_y = guessed.y.iloc[0]
         writer.goto(write_x, write_y)
         writer.write(answer_state, font=("Arial", 10, "bold"))
-    elif answer_state is None:
-        quiz_on = False
-        break
+
 
 
 screen.exitonclick()
