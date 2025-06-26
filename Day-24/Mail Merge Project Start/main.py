931c7c587264aa5c5d49bda181a4954e6ff4e2e9
brother-samuel <samuel@hesperion.cz>
2025-06-26 11:45:51 +0200

    Day 24 Finished

--- Day-24/Mail Merge Project Start/main.py
+++ Day-24/Mail Merge Project Start/main.py
@@ -1,4 +1,3 @@
-#TODO: Create a letter using starting_letter.txt
 template = open("Input/Letters/starting_letter.txt")
 letter = template.read()
 glist = open("Input/Names/invited_names.txt")
@@ -11,12 +10,3 @@ for guest in invitees:
 template.close()
 glist.close()
 
-
-#print(template.read())
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
